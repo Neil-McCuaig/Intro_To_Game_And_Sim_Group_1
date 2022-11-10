8b3b1d37dcc6012018d6fbbb0684aacc9b865f71
Lyndsay Riches <lriches1@my.centennialcollege.ca>
2022-11-10 11:57:28 -0500

    Added walls, started adding roads and player spawn bunkers

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -20,7 +20,7 @@ public class Rotation : MonoBehaviour
     void Update()
     {
         move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        rotation = Input.GetAxis("Horizontal") * - rotateSpeed * Time.deltaTime;
     }
 
     private void LateUpdate()
